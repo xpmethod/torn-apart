8bae471ef05cf6fca05a38ce4fc9fdcb962ab936
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-06-22 08:47:31 -0400

    Add imgur to google grabber

--- google-sat.rb
+++ google-sat.rb
@@ -1,14 +1,16 @@
 require "dotenv"
 require "httparty"
 require "csv"
+require "./imgur"
 
 api = ENV["GOOGLE_MAPS_API"]
 # zoom = 19 # slightly bigger than single-apt-building view
 zoom = 18 # slightly bigger than single-apt-building view
 
 # detention_centers = CSV.read("data/detention.csv", { headers: true })
-ice_centers = CSV.read("docs/web-data/ice-facs_geocoded.csv", { headers: true })
+ice_centers = CSV.read("docs/assets/data/ice-facs_geocoded.csv", { headers: true })
 
+puts "filename,imgur"
 # detention_centers.each do |center|
 ice_centers.each do |center|
   lat = center["lat"].to_f
@@ -17,14 +19,20 @@ ice_centers.each do |center|
   # lon = center["Longitude"].to_f
   unless lat == 0.0 && lon == 0.0
     latlng = "#{lat},#{lon}"
-    id = latlng.sub(/,/, "")
-    puts "Getting image for #{lat}, #{lon} (#{center["Name"]})"
+    t = Time.now.to_i
+    filename = "#{center["DETLOC"]}-#{t}.png"
+    # puts "Getting image for #{lat}, #{lon} (#{center["Name"]})"
     response = HTTParty.get("https://maps.googleapis.com/maps/api/staticmap?center=#{latlng}&zoom=#{zoom}&size=640x640&maptype=satellite&key=#{api}")
-    puts "Saving image as sat-#{id}.png"
-    File.open("docs/imgs/ice-#{center["DETLOC"]}-#{id}.png", "wb") do |f|
-    # File.open("docs/imgs/sat-#{id}.png", "wb") do |f|
+    # puts "Saving image as sat-#{id}.png"
+    File.open("localdata/#{filename}.png", "wb") do |f|
       f.write response.body
     end
+    imgur_url = upload_image filename
+    unless imgur_url
+      refresh_token
+      imgur_url = upload_image filename
+    end
+    puts "#{filename},#{imgur_url}"
     sleep 5
   end
 end
