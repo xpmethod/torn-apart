f4becf31417280124a5070956889c8658a010a2a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-06-23 19:27:00 -0400

    Remove some of the commented chaos.

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -59,112 +59,6 @@ $( document ).ready(() => {
   //   g = svg.append("g").attr("class", "leaflet-zoom-hide");
   // const blackSites = [];
 
-  /*
-  d3.json("web-data/blacksites.json", (error, collection) => {
-  if (error) throw error;
-  // d3 is very clever w/ geojson (paths and transforms), but if we want
-  // to simply take latlngs and make them into points we can 
-  // subsequently build on, we have to get a bit craftier.
-  const blackSites = [{}, {}, {}, {}];
-  // collection.features.forEach(feature => {
-  //   feature.LatLng = new L.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
-  //   for(let i = 1; i <= feature.properties.count; i++) {
-  //     blackSites.push({dco: feature.properties.dco, latLng: feature.LatLng});
-  //   }
-  // });
-  // console.log(blackSites);
-  const simulation = d3.forceSimulation(blackSites)
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter($(window).width / 2, $(window).height / 2))
-    .on("tick", ticked);
-
-
-  // function ticked() {
-  //   var u = d3.select('svg')
-  //     .selectAll('circle')
-  //     .data(blackSites)
-
-  //   u.enter()
-  //     .append('circle')
-  //     .attr('r', 5)
-  //     .merge(u)
-  //     .attr('cx', function(d) {
-  //       return d.x
-  //     })
-  //     .attr('cy', function(d) {
-  //       return d.y
-  //     })
-
-  //   u.exit().remove()
-  // }
-  
-  // collection.features.forEach(feature => {
-  //   console.log("now drawing for", feature.properties.dco);
-  //   console.log("should be this many nodes:", feature.properties.count);
-  //   feature.LatLng = new L.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
-  //   const nodes = d3.range(feature.properties.count).map(() => { return {type: 1}; });
-  //   const dots = g.selectAll("circle")
-  //     .data(nodes)
-  //     .enter().append("circle")
-  //     .classed(feature.properties.dco, true)
-  //     .attr("r", 5)
-  //     .attr("fill", "#333");
-  //   const simulation = d3.forceSimulation(nodes)
-  //     .force("charge", d3.forceCollide().radius(10))
-  //     .force("r", d3.forceRadial(20, map.latLngToLayerPoint(feature.LatLng).x, map.latLngToLayerPoint(feature.LatLng).y ))
-  //     .on("tick", ticked);
-
-  //   function ticked() {
-  //     dots.attr("cx", d => d.x)
-  //       .attr("cy", d => d.y);
-  //   }
-  // });
-  // const features = g.selectAll("text")
-  //   .data(collection.features)
-  //   .enter().append("text")
-  //   .attr("text-anchor", "middle")
-  //   .attr("font-family", "sans-serif")
-  //   .attr("font-size", "18")
-  //   .attr("fill", "#333")
-  //   .attr("id", d => d.properties.dco)
-  //   // .attr("opacity", 0.0)
-  //   .classed("dco", true)
-  //   .text(d => d.properties.dco);
-
-  // const 
-  //   console.log(nodes.length);
-  //   console.log(feature.properties.dco);
-
-  // });
-
-  // map.on("viewreset", reset());
-  // map.on("zoomend", reset);
-  // reset();
-
-  // function reset() {
-  //   features.attr("transform", d => `translate(${map.latLngToLayerPoint(d.LatLng).x},${map.latLngToLayerPoint(d.LatLng).y})`);
-  // }
-  });
-  */
-
-
-  /*
-  // Now add the other layers.
-  [[crossing, "#00dd00"], [detention, "#dd0000"]].forEach( geojson => {
-    let popup;
-    L.geoJSON(geojson[0], {
-      pointToLayer(f, l) {
-        if (geojson[0] === detention) {
-          const latlng = `${l.lat}${l.lng}`;popup = `<div class="row"><div class="col-6"><h4>${f.properties["Name"]}</h4></div><div class="col-6"><img class="img-fluid" alt="${f.properties["Name"]} satellite photo" src="imgs/sat-${latlng}.png"></div></div>`;
-        } else {
-          popup = `<h4>${f.properties["Name"]}</h4>`;
-        }
-        return L.circleMarker(l, {fillColor: geojson[1], color: geojson[1]}).bindPopup(popup);
-      }
-    })//.addTo(map);
-  });
-  */
-
   $(".locale-button").click(function(e){
     e.preventDefault();
     $(".locale-button").removeClass("active");
@@ -183,10 +77,6 @@ $( document ).ready(() => {
     $( this ).text(langs[index]);
     update_texts();
   });
-
-    
-
-    
 
 }); // close document.ready()
 
