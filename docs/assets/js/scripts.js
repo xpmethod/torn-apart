e1e25b2737ab8ca0dd6cdcd0ba9e59adf36dc810
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-06-23 19:42:52 -0400

    Clean up legend and change default viz to dots.

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -348,9 +348,9 @@ function buildPointsLegend(){
     return `<div class="row px-3 pt-2">
       <div class="col-sm-4">
         <div class="media">
-          <svg class="m-2" height="${defaultRadius * 4}" width="${defaultRadius * 4}">
+          <svg class="m-2" height="${defaultRadius * 5}" width="${defaultRadius * 5}">
             <circle r="${defaultRadius * 1.5}" 
-              cx="${defaultRadius * 2}" cy="${defaultRadius * 2}" 
+              cx="${defaultRadius * 2.5}" cy="${defaultRadius * 2.5}" 
               fill="${purple}" fill-opacity="0.8"
               stroke="black" stroke-width="1" stroke-opacity="0.8" />
           </svg>
@@ -361,9 +361,9 @@ function buildPointsLegend(){
       </div>
       <div class="col-sm-4">
         <div class="media">
-          <svg class="m-2" height="${defaultRadius * 4}" width="${defaultRadius * 4}">
+          <svg class="m-2" height="${defaultRadius * 5}" width="${defaultRadius * 5}">
             <circle r="${defaultRadius * 2}" 
-              cx="${defaultRadius * 2}" cy="${defaultRadius * 2}" 
+              cx="${defaultRadius * 2.5}" cy="${defaultRadius * 2.5}" 
               fill="${orange}" fill-opacity="0.8"
               stroke="black" stroke-width="1" stroke-opacity="0.8" />
           </svg>
@@ -374,9 +374,9 @@ function buildPointsLegend(){
       </div>
       <div class="col-sm-4">
         <div class="media">
-          <svg class="m-2" height="${defaultRadius * 4}" width="${defaultRadius * 4}">
+          <svg class="m-2" height="${defaultRadius * 5}" width="${defaultRadius * 5}">
             <circle r="${defaultRadius}" 
-              cx="${defaultRadius * 2}" cy="${defaultRadius * 2}" 
+              cx="${defaultRadius * 2.5}" cy="${defaultRadius * 2.5}" 
               fill="${orange}" fill-opacity="0.8"
               stroke="black" stroke-width="1" stroke-opacity="0.8" />
           </svg>
