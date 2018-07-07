62714733b416808fbd54a7420fbd5ff375e2a9b5
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-07 12:07:27 -0400

    Turn news-sniffer into a class

--- news-sniffer/news-sniffer.rb
+++ news-sniffer/news-sniffer.rb
@@ -2,19 +2,28 @@ require "csv"
 require "news-api"
 require "dotenv"
 
-newsapi = News.new(ENV["GOOGLE_NEWS_API"])
+class NewsSniffer 
 
-ice_facs = CSV.read("../docs/assets/data/iceFacs.csv", { headers: true })
-news_sources = CSV.read("../data/news-crawl-output.csv", {headers: true })
-states = news_sources.map{ |source| source["state"] }.uniq
-state_source_domains = states.map do |state|
-  { state: state,
-    sources: news_sources.select{ |news_source| news_source["state"] == state }.map{ |source|
-      URI.parse(source["link"]).host.downcase.gsub(/^www\./, "")
-    }
-  }
+  def initialize 
+    @newsapi = News.new(ENV["GOOGLE_NEWS_API"])
+    @ice_facs = CSV.read("../docs/assets/data/iceFacs.csv", { headers: true })
+  end
+
+  def create_domains_by_state
+    news_sources = CSV.read("../data/news-crawl-output.csv", {headers: true })
+    states = news_sources.map{ |source| source["state"] }.uniq
+    state_source_domains = states.map do |state|
+      { state: state,
+        sources: news_sources.select{ |news_source| news_source["state"] == state }.map{ |source|
+          URI.parse(source["link"]).host.downcase.gsub(/^www\./, "")
+        }
+      }
+    end
+    File.open("../data/news-sources-by-state.json", "w") do |file|
+      file.puts state_source_domains.to_json
+    end
+  end
+
 end
-File.open("../data/news-sources-by-state.json", "w") do |file|
-  file.puts state_source_domains.to_json
-end
+
 
