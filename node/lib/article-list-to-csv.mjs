c5d275ff5f1e3deb3c036fe38aea94f7a6892888
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-09 00:28:02 -0400

    cet state-articles.csv to escape quotes properly

--- node/lib/article-list-to-csv.mjs
+++ node/lib/article-list-to-csv.mjs
@@ -17,12 +17,17 @@ export default function() {
               const json = JSON.parse(file);
               if (json.articles.length > 0){
                 json.articles.map( article => {
+                  const fields = [article.source.name, article.author, article.title, article.description].map( field => {
+                    if (field) {
+                      return field.replace(/"/g, "“");
+                    }
+                  });
                   stdout.write(JSON.stringify([dir, 
                     report.replace(".json", ""),
-                    article.source.name,
-                    article.author,
-                    article.title,
-                    article.description,
+                    fields[0],
+                    fields[1],
+                    fields[2],
+                    fields[3],
                     article.url
                   ]).replace(/^\[/, "").replace(/\]$/, "") + "\n");
                 });
