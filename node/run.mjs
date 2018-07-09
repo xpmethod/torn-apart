5549f236bef35acf797fb61a26d10cffdb5606f8
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-09 13:06:08 -0400

    Get the runner ready to slay.

--- node/run.mjs
+++ node/run.mjs
@@ -1,4 +1,7 @@
 // import articleCounter from "./lib/article-counter.mjs";
-import articleListToCSV from "./lib/article-list-to-csv.mjs";
+// import articleListToCSV from "./lib/article-list-to-csv.mjs";
 
-articleListToCSV();
+// articleListToCSV();
+import newsSnifferByState from "./lib/news-sniffer-by-state";
+
+newsSnifferByState();
