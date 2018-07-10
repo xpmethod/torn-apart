54509fb57d688abb297e487c2e8e040e926e76e0
Alex Gil <colibri.alex@gmail.com>
2018-07-09 23:16:40 -0400

    new news

--- node/run.mjs
+++ node/run.mjs
@@ -1,11 +1,11 @@
-import newsSnifferByState from "./lib/news-sniffer-by-state";
-newsSnifferByState();
+// import newsSnifferByState from "./lib/news-sniffer-by-state";
+// newsSnifferByState();
 
 // To make the states-articles.csv, first, comment the two lines above this.
 // Next, uncomment these two lines:
 //
-// import articleListToCSV from "./lib/article-list-to-csv.mjs";
-// articleListToCSV();
+import articleListToCSV from "./lib/article-list-to-csv.mjs";
+articleListToCSV();
 //
 // Then, in the shell, run this command:
 //
