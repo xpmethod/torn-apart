6d2a14c9cc708908147bb12ba99996adcefc52aa
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-13 21:27:04 -0400

    move trap to d3.

--- src/trap.js
+++ src/trap.js
@@ -3,11 +3,11 @@ import trapLegend from "./trap-legend";
 import trapLayer from "./trap-layer";
 
 export default function(map){
-  const layer = trapLayer();
-  layer.addTo(map);
   map.dragging.enable();
-  map.flyToBounds([[24.396, -124.848974], [49.384, -66.885444]]);
   $(".leaflet-control-zoom").show();
   map.flyToBounds([[34.1638, -97.1375], [25.8439, -118.608244]]);
+  $(map.getPanes().overlayPane).append("<svg id='d3-trap-svg'></svg>");
+  const layer = trapLayer(map);
+  layer.addTo(map);
   trapLegend();
 }
