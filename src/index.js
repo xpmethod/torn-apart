62092661f7ee2038262f7fb9681b8faefba03a6f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-13 20:05:14 -0400

    Separate out clinks for index.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import $ from "jquery";
 import initMap from "./map-init";
-import showViz from "./show-viz";
+import clinks from "./clinks";
 
 const map = initMap("mapdiv");
 $("#indexModal").modal("show");
 $("#legend").click(function(){ $(this).hide(); });
-showViz("clinks", map);
+clinks(map);
 
