1587a9cd14e97897bd6fbd5858f9f872384055c0
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-15 19:15:45 -0400

    fix linting errors.

--- src/update-texts.js
+++ src/update-texts.js
@@ -10,23 +10,18 @@ function currentLocaleToggle(locale){
 export default function() {
   const md = markdownit({html: true}).use(markdownitFootnote);
   const externalLinkHTML = "<span>&nbsp;<i style='vertical-align: baseline; font-size: 60%;' class='fa fa-small fa-external-link-alt'></i></span>";
-  // const q = d3.queue();
-  // q.defer(callback => {$("body").i18n(); callback(null);});
   $("body").i18n();
-  // q.await(function(e){
-  //   if (e) throw e;
-    currentLocaleToggle($.i18n().locale);
-    $(".markdownify").html((i, html) => {
-      return md.render(html);
-    });
-    $(".click-to-hide a").each(function() {
-      $( this ).attr("onclick", "event.stopPropagation();");
-    });
-    $("a[href^='http']:not(a:has(img))").html(function(i, html){
-      if(!html.match("fa-external-link-alt")){
-        $( this ).append($.parseHTML(externalLinkHTML));
-      }
-    });
-    $("a[href^='http']").attr("target", "_blank");
-  // });
+  currentLocaleToggle($.i18n().locale);
+  $(".markdownify").html((i, html) => {
+    return md.render(html);
+  });
+  $(".click-to-hide a").each(function() {
+    $( this ).attr("onclick", "event.stopPropagation();");
+  });
+  $("a[href^='http']:not(a:has(img))").html(function(i, html){
+    if(!html.match("fa-external-link-alt")){
+      $( this ).append($.parseHTML(externalLinkHTML));
+    }
+  });
+  $("a[href^='http']").attr("target", "_blank");
 }
