1152a6510ec6d1a46ea3b552a9170644a7ee876e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-18 08:21:58 -0400

    Node is async you idiot.

--- src/news/get-total-articles-per-domain.js
+++ src/news/get-total-articles-per-domain.js
@@ -1,5 +1,5 @@
 import NewsAPI from "newsapi";
-import { fileWriteCSV } from "../file-utils";
+import { createWriteStream } from "fs";
 import { resolve, join } from "path";
 import _ from "lodash";
 import { fileLinesToArray } from "../file-utils";
@@ -7,18 +7,17 @@ import { fileLinesToArray } from "../file-utils";
 export default function (apiKey, teamName) {
   const newsapi = new NewsAPI(apiKey);
   const dataPath = resolve("data", "google-index");
-  const results = fileLinesToArray(join(dataPath, `${teamName}-team.txt`))
-    .map(source => { return { domain: source }; });
-  _.each(results, source => {
-    if (source.domain) {
+  const output = createWriteStream(join(dataPath, `${teamName}-team-with-count.csv`), {flags: "a" });
+  output.write("domain,totalResults\n");
+  _.each(fileLinesToArray(join(dataPath, `${teamName}-team.txt`)), domain => {
+    if (domain.length > 0) {
       newsapi.v2.everything({
         pageSize: 100,
-        domains: source.domain
+        domains: domain
       }).then( response => {
-        source.totalResults = response.totalResults;
+        output.write(`${domain},${response.totalResults}\n`); 
       });
     }
   });
-  fileWriteCSV(join(dataPath, `${teamName}-team-with-count.csv`), results);
 }
 
