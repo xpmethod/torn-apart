019fb70090222d74b12d4e968b9fd12789c71b84
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-17 21:26:19 -0400

    Set up totalrequester.

--- src/news/api-key.js
+++ src/news/api-key.js
@@ -1,15 +1,16 @@
 import config from "../environment";
 // import readline from "readline";
+// import { stdin, stdout } from "process";
 import { stdout } from "process";
-import queryDomains from "./query-domains";
+// import queryDomains from "./query-domains";
+import getTotalArticlesPerDomain from "../news/get-total-articles-per-domain";
 
 export default function(emitter){
   if (config.newsApiKey) {
     stdout.write(`\nIt looks like you have a Google News API key registered with this app.
       \nThat key is: ${config.newsApiKey}\n`); 
-    emitter;
-    // emitter.emit("getApiKey", config.newsApiKey);
-  // This worked all of once...
+    emitter.emit("getApiKey", config.newsApiKey);
+    getTotalArticlesPerDomain(config.newsApiKey, "red");
   // } else {
   //   const rl = readline.createInterface(stdin, stdout);
   //   rl.question("Cannot find a Google News API key. Please get one at http://newsapi.org.\n\nThen, enter it here > ", answer => {
@@ -17,5 +18,4 @@ export default function(emitter){
   //     emitter.emit("getApiKey", answer);
   //   });
   }
-  queryDomains(config.newsApiKey);
 }
