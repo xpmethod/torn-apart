c8cd4db6eb283ee52e76ed295263752c9941c3be
RHendery <hendery@gmail.com>
2018-07-23 19:06:16 +1000

    dots scale to svg size, and spacing of clusters adjusted

--- src/galaxy/draw-bubbles.js
+++ src/galaxy/draw-bubbles.js
@@ -9,24 +9,27 @@ import { format } from "d3-format";
 export default function(){  
   const width = $(window).width() - 4 * rem;
   const height = $("#v2-div").position().top + $("#v2-div").height() - $("#galaxy-svg").position().top;
+  const scaling = width*0.000767; //this scales the dots radius appropriately for different widths of svg
+  console.log(String(width));
+  const scaled_width = width*0.8; //this is the scaling factor for determining the centres of each cluster
 
-  var xCenter = {2014: width/6, 2015: width/6*2, 2016: width/6*3, 2017: width/6*4, 2018: width/6*5}; //this dictionary allows us to grab an x-coordinate to have a node pushed towards, on the basis of its financial year (from a column I added to the data, since each financial year was handled by different worksheets in the original spreadsheet).
+  var xCenter = {2014: scaled_width*14/155+width/15, 2015: scaled_width*(14+22)/155+width/15, 2016: scaled_width*(14+22+31)/155+width/15, 2017: scaled_width*(14+22+31+42)/155+width/15, 2018: scaled_width*(14+22+30+42+47)/155+width/15}; //this dictionary allows us to grab an x-coordinate to have a node pushed towards, on the basis of its financial year (from a column I added to the data, since each financial year was handled by different worksheets in the original spreadsheet). The values for the centres of each cluster are determined by approximating the cluster as a circle, assuming similar distribution of dots in each, and figuring out from the number of nodes per FY the proportional differences in radius between each, then kind of handwaving the rest and fiddling with the final term until it looks okay. At the very minimum, we should probably replace the hardcoded number of nodes for each FY with the number as grabbed from the data dynamically.
 
   var uniqueness_colour = {"multi-year": green, "unique": purple}; //a dictionary here is overkill, but just in case you want to change it to a more nuanced set of colours, e.g. for the 'year' column instead.
-
+  
   var simulation = forceSimulation(Data) 
     .force("x", forceX().strength(1).x(function(d) {
       return xCenter[d.financial_year];
     }))
     .force("y", forceY(height/1.8))
     .force("collision", forceCollide().radius(function(d) {
-      return (Math.ceil(Math.sqrt(d.current_total_value_of_award)/600+8)); // if you want more space around the larger dots (i.e. padding as proportional to radius), increase the number you divide by (currently 600). If you want more padding around all dots evenly, increase the additional constant (+5)
+      return (Math.ceil(scaling*(Math.sqrt(d.current_total_value_of_award)/600+8))); // if you want more space around the larger dots (i.e. padding as proportional to radius), increase the number you divide by (currently 600). If you want more padding around all dots evenly, increase the additional constant (+8). The ceil is to make sure we don't end up with anything under 1 pixel radius.
     }))
 
     .stop();//stop the simulation here. This means it doesn't do all its initial stuff in the public eye.
   
 
-  for (var i = 0; i < 15; ++i) simulation.tick(); //this is now making the simulation run a few times without drawing anything, so you don't get all the wibbly wobbly.The higher you make this number, the more clustered the result will be but the longer it will take to load
+  for (var i = 0; i < 15; ++i) simulation.tick(); //this is now making the simulation run a few times without drawing anything, so you don't get all the wibbly wobbly.The higher you make this number, the more clustered the result will be but the longer it will take to load (currently 15)
 
   var labels = [2014, 2015, 2016, 2017, 2018];
 
@@ -51,7 +54,7 @@ export default function(){
       return "FY " + d;
     })
     .attr("x", function(d){
-      return xCenter[d] -50;
+      return xCenter[d] -50; //this -50 needs to be replaced with something proportional to the text size
     })
     .attr("y", 20)
     .attr("class", "label");
@@ -60,8 +63,8 @@ export default function(){
     .data(Data);
   node.enter().append("circle")
     .style("fill", d => uniqueness_colour[d.uniqueness])
-    .attr("r", function(d) { return (Math.ceil(Math.sqrt(d.current_total_value_of_award)/700+2)); })
-    // .merge(node)
+    .attr("r", function(d) {return scaling*(Math.ceil(Math.sqrt(d.current_total_value_of_award)/700+2)); 
+	})
     .attr("cx", function(d) {
       return d.x;
     })
@@ -73,7 +76,7 @@ export default function(){
       d.tooltip = `<strong>${d.recipient_name}</strong><br />
     &#36;${format(",")(Math.round(d.current_total_value_of_award))}`; //rounded to nearest whole number, because the period before the cents was hard to see and made the numbers look bigger than they really are, plus it was doing .4 and .3 instead of .40, .30, etc.
       d.mouseOver = () => {
-        select(`#circle-${d.id}`) // Trying to pull from ID but still failing
+        select(`#circle-${d.id}`) 
           .attr("filter", "url(#filter-glow)");
       };
       d.mouseOut = () => {
