9d207089ca25e4c1c149dbad7289dbce829c07c8
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-23 23:55:46 -0400

    MAke some initial style changes.

--- src/galaxy/draw-bubbles.js
+++ src/galaxy/draw-bubbles.js
@@ -9,13 +9,33 @@ import { format } from "d3-format";
 export default function(){  
   const width = $(window).width() - 4 * rem;
   const height = $("#v2-div").position().top + $("#v2-div").height() - $("#galaxy-svg").position().top;
-  const scaling = width*0.000567; //this scales the dots radius appropriately for different widths of svg
+  const scaling = width*0.000567; //this scales the dots radius appropriately 
+  // for different widths of svg
   // console.log(String(width));
-  const scaled_width = width*0.8; //this is the scaling factor for determining the centres of each cluster
+  const scaled_width = width*0.8; //this is the scaling factor for 
+  // determining the centres of each cluster
 
-  var xCenter = {2014: scaled_width*4/155+width/15, 2015: scaled_width*(12+22)/155+width/15, 2016: scaled_width*(14+22+31)/155+width/15, 2017: scaled_width*(12+22+31+42)/155+width/15, 2018: scaled_width*(12+22+30+42+47)/155+width/15}; //this dictionary allows us to grab an x-coordinate to have a node pushed towards, on the basis of its financial year (from a column I added to the data, since each financial year was handled by different worksheets in the original spreadsheet). The values for the centres of each cluster are determined by approximating the cluster as a circle, assuming similar distribution of dots in each, and figuring out from the number of nodes per FY the proportional differences in radius between each, then kind of handwaving the rest and fiddling with the final term until it looks okay. At the very minimum, we should probably replace the hardcoded number of nodes for each FY with the number as grabbed from the data dynamically.
-
-  var uniqueness_colour = {"multi-year": green, "unique": purple}; //a dictionary here is overkill, but just in case you want to change it to a more nuanced set of colours, e.g. for the 'year' column instead.
+  // this dictionary allows us to grab an x-coordinate to have a node pushed
+  // towards, on the basis of its financial year (from a column I added to the
+  // data, since each financial year was handled by different worksheets in the
+  // original spreadsheet). The values for the centres of each cluster are
+  // determined by approximating the cluster as a circle, assuming similar
+  // distribution of dots in each, and figuring out from the number of nodes per
+  // FY the proportional differences in radius between each, then kind of
+  // handwaving the rest and fiddling with the final term until it looks okay.
+  // At the very minimum, we should probably replace the hardcoded number of
+  // nodes for each FY with the number as grabbed from the data dynamically.
+  var xCenter = { 
+    2014: scaled_width*4/155+width/15, 
+    2015: scaled_width*(12+22)/155+width/15, 
+    2016: scaled_width*(14+22+31)/155+width/15, 
+    2017: scaled_width*(12+22+31+42)/155+width/15, 
+    2018: scaled_width*(12+22+30+42+47)/155+width/15
+  };
+  
+  // a dictionary here is overkill, but just in case you want to change it to a
+  // more nuanced set of colours, e.g. for the 'year' column instead.
+  var uniqueness_colour = {"multi-year": green, "unique": purple}; 
   
   var simulation = forceSimulation(Data) 
     .force("x", forceX().strength(0.8).x(function(d) {
@@ -25,11 +45,15 @@ export default function(){
     .force("collision", forceCollide().radius(function(d) {
       return (Math.ceil(scaling*(Math.sqrt(d.current_total_value_of_award)/600+11))); // if you want more space around the larger dots (i.e. padding as proportional to radius), increase the number you divide by (currently 600). If you want more padding around all dots evenly, increase the additional constant (+8). The ceil is to make sure we don't end up with anything under 1 pixel radius.
     }))
-
-    .stop();//stop the simulation here. This means it doesn't do all its initial stuff in the public eye.
+    .stop();
+  // stop the simulation here. This means it doesn't do all its initial stuff
+  // in the public eye.
   
-
-  for (var i = 0; i < 15; ++i) simulation.tick(); //this is now making the simulation run a few times without drawing anything, so you don't get all the wibbly wobbly.The higher you make this number, the more clustered the result will be but the longer it will take to load (currently 15)
+  //this is now making the simulation run a few times without drawing anything,
+  //so you don't get all the wibbly wobbly.The higher you make this number, the
+  //more clustered the result will be but the longer it will take to load
+  //(currently 15)
+  for (var i = 0; i < 15; ++i) simulation.tick(); 
 
   var labels = [2014, 2015, 2016, 2017, 2018];
 
@@ -75,7 +99,7 @@ export default function(){
     })
     .attr("id", d => `circle-${d.financial_year}-${d.award_id_piid}`)
     .on("mouseover", handleMouseOver)
-    .on("mouseout", handleMouseOut)  
+    .on("mouseout", handleMouseOut);  
     
 
   var text = svg
@@ -88,7 +112,8 @@ export default function(){
       return "FY " + d;
     })
     .attr("x", function(d){
-      return xCenter[d] -30; //this -50 needs to be replaced with something proportional to the text size
+      //this -50 needs to be replaced with something proportional to the text size
+      return xCenter[d] - 30; 
     })
     .attr("y", 20)
     .attr("class", "label");
