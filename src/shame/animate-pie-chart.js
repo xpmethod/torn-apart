597de5dcf47a47d06ea50de93ec2eff9e719877d
RHendery <hendery@gmail.com>
2018-07-27 09:58:50 +1000

    dynamic labels on pie chart

--- src/shame/animate-pie-chart.js
+++ src/shame/animate-pie-chart.js
@@ -5,8 +5,8 @@ import "d3-transition";
 
 export default function(){
 	
-  //I don't know why, since I'm not using this data, but I somehow have to create and bind some data to the text thing else nothing shows up.
-  const labelData = [ { "pie": "gender"}];
+  //I don't know why, since I'm not using this data, but I somehow have to create and bind some data to the text thing else no text shows up later.
+  const labelData = [ { "key": "value"}];
 	
   // choose the slice that was clicked.
   const slice = select(this);
@@ -29,26 +29,37 @@ export default function(){
       .enter()
       .append("text");
 	
-    //I define segmentLabel as the taName for the currently clicked segment (because I can add this to "<ta-v2-" and ">" later to get the class for translating. And I set labelColor to the current segment colourr
+    //I define segmentLabel as the taName for the currently clicked segment (because I can add this to "<ta-v2-" and ">" later for translation purposes. And I set labelColor to the current segment colour
     var segmentLabel = `${slice.attr("taName")}`;
     var labelColor = `${slice.attr("fill")}`;
 
-    //This actually creates a text label (but seemingly only once!? wtf? Not every time I click. The text is set to "segmentLabel" with the color "labelColor"
+    //This actually creates a text label. The text is set to "segmentLabel" with the color "labelColor"
     textsvg
       .attr("x", 0)
       .attr("y", 20)
       .attr("class", "dynamic-legend")
+	  .attr("id", "text-container")
       .style("font-size", "1rem")
       .style("font-weight", "bold")
       .attr("fill", function() { return labelColor; })
-    //obviously this next bit is generating only the inside bit of the data-i18n="" thingo at the moment. Once it's working properly, it needs to go inside the <text> thing, I think.
-      .text( function () {var textlabel = "ta-v2-" + segmentLabel; return textlabel; });
+    //obviously this next bit is generating only the inside bit of the data-i18n="" thingo at the moment. I'm just printing it to the page for now.
+      .text(function () {var textlabel = "ta-v2-" + segmentLabel; return textlabel; }); //function () {var textlabel = "ta-v2-" + segmentLabel; return textlabel; })
 
-    //This looks like code for what happens when you unclick something?
+	 
+	  
+
+    //And if you de-click something, then:
   } else {
     slice.transition().delay(0).duration(250)
       .style("stroke-width", 0)
       .style("opacity", "0.7");
+	  
+//this now removes the legend item when de-clicked
+	  select("#text-container")	
+      .remove("text");
  
+ //this sets the state to 'unclicked' so if you take this out it will never let you select anything else, dummy.
+	selectAll(".clicked")
+        .classed("clicked", false);
   }
 }
