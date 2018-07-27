f8cf2325bbae4407a45fd96d3e161fc2c40386fa
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-27 00:07:34 -0400

    Get the basics of the treemap working.

--- src/explorer.js
+++ src/explorer.js
@@ -1,6 +1,12 @@
 import $ from "jquery";
+// import explorerBlocks from "./explorer/blocks";
+import explorerTree from "./explorer/tree";
 
 export default function(){
   $("#v2-div").show();
-  $("#explorer-viz").show();
+  // explorerBlocks();
+  $(document).ready(() => {
+    explorerTree();
+    $("#explorer-viz").show();
+  });
 }
