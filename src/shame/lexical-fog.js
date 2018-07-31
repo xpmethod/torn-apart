6f28d3e2c86fc168accac4e1c9878cd90810ba31
Gregory Palermo <gregoryjpalermo@gmail.com>
2018-07-31 16:36:42 -0400

    lexical-fog tooltips
    added tooltips to the word cloud that are formatted as $<amount>.00

--- src/shame/lexical-fog.js
+++ src/shame/lexical-fog.js
@@ -9,30 +9,27 @@ import * as cloud from "d3-cloud";
 import * as colorscale from "d3-scale-chromatic";
 import { format } from "d3-format";
 
-//still needs a lot of formatting work, colouring from something in the data, and adding tooltips or something so you can get info about each org/contract. Maybe html links from each org name to the org"s "contact us" page or direct to email links? So you can write and complain easily. 
+//still needs a lot of formatting work, colouring from something in the data, and adding tooltips or something so you can get info about each org/contract. Maybe html links from each org name to the org"s "contact us" page or direct to email links? So you can write and complain easily.
 //still TODO - need to style the tooltips and bring them to the top
 
-export default function(chart){  
+export default function(chart){
 
   const width = chart.width;
   const height = chart.height;
 
-  var colour = colorscale.schemeSet2; 
+  var colour = colorscale.schemeSet2;
   var svg = select("#lexical-fog-svg")
     .attr("class", "wordcloud")
     .append("g")
     // without the transform, words would get cutoff to the left and top, they would
     // appear outside of the SVG area
-    .attr("transform", "translate("+ width/2 + "," + height/2 + ")");  
+    .attr("transform", "translate("+ width/2 + "," + height/2 + ")");
 
-  //Create a tooltip - Greg: perhaps this can be where we create the single text box
-  var tip = svg.append("g")
-    .attr("id","cloud_tooltip")
-    .attr("position","absolute");
+
 
   cloud().size([width, height-height*0.2])
     .words(Data)
-    .text(function(d) { return d.name; }) 
+    .text(function(d) { return d.name; })
     .rotate(0)
     .fontSize(function(d) {return (Math.ceil(Math.pow(d.total_value, 0.26))/4 +6); })
     .padding(1)
@@ -40,6 +37,7 @@ export default function(chart){
     .on("end", draw)
     .start();
 
+  //place to bind on mouseover to words
   //Draw the words of the word cloud
   function draw(words) {
     svg.selectAll("text")
@@ -48,48 +46,65 @@ export default function(chart){
       .attr("text-anchor", "middle")
       .style("font-family", "Impact")
       .style("font-size", function(d) { return (Math.ceil(Math.pow(d.total_value, 0.26))/4 +4) + "px"; })
-      .style("fill", function(d) {return colour[5-d.years];}) 
+      .style("fill", function(d) {return colour[5-d.years];})
       .attr("transform", function(d) {
         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
       })
-      .text(function(d) { return d.name; })
+      .text(function(d) {
+        return d.name;
+      })
+      .each(d => {
+        d.id = `${d.name}`;
+        d.tooltip = `<strong>${format("$,.2f")(d.total_value)}`;
+        d.mouseOver = () => {
+          select(`#text-${d.id}`);
+        };
+        d.mouseOut = () => {
+          select(`#text-${d.id}`);
+        };
+      })
       .on("mouseover", handleMouseOver) //call the mouse-over handler
       .on("mouseout", handleMouseOut); //call the mouse-out handler
   }
-  
-  //Hovering handlers
-  function handleMouseOver(d){
-    //Lighten the text of the wordcloud
-    select(this).style("opacity",0.7);
-    //Show tooltip
-    var base = d.y - d.size; //Calculate position of text
 
-    //Position and populate the text of the tooltip
-    tip.append("text")
-      .attr("id","cloud_tooltip_text")
-      .attr("x",d.x) //position x - this could be static
-      .attr("y",base) //position y
-      .attr("text-anchor", "middle")
-      .style("font-size","1.25rem")
-      .style("fill", colour[5-d.years])
-      .text("$" + format(",")(Math.round(d.total_value))); //Format the text as $
-
-    //Create and position the bounding box behind the text
-    tip.append("rect")
-      .attr("id","cloud_tooltip_rect")
-      .attr("width",select("#cloud_tooltip_text").node().getBBox().width)
-      .attr("height",select("#cloud_tooltip_text").node().getBBox().height)
-      .attr("x",select("#cloud_tooltip_text").node().getBBox().x)
-      .attr("y",select("#cloud_tooltip_text").node().getBBox().y)
-      .attr("fill","black")
-      .style("opacity",0.3);
-  }
-  function handleMouseOut() {
-    select(this)
-      .style("opacity",1);
-      
-    //Remove tooltip
-    select("#cloud_tooltip_text").remove(); //get rid of tooltip text
-    select("#cloud_tooltip_rect").remove(); //get rid of the tooltip rect
-  }
+// Below is Alex Ahmed's code, which she wrote before she knew we were using Moacir's tooltips
+  // //Hovering handlers
+  // function handleMouseOver(d){ //don't need to be redefined, because we need visual consistency
+  //   //Lighten the text of the wordcloud
+  //   select(this).style("opacity",0.7);
+  //   //Show tooltip
+  //   var base = d.y - d.size; //Calculate position of text
+  //
+  //   //Position and populate the text of the tooltip
+  //   tip.append("text")
+  //     .attr("id","cloud_tooltip_text")
+  //     .attr("x",d.x) //position x - this could be static
+  //     .attr("y",base) //position y
+  //     .attr("text-anchor", "middle")
+  //     .style("font-size","1.25rem")
+  //     .style("fill", colour[5-d.years])
+  //     .text("$" + format(",")(Math.round(d.total_value))); //Format the text as $
+  //
+  //   //Create and position the bounding box behind the text
+  //   tip.append("rect")
+  //     .attr("id","cloud_tooltip_rect")
+  //     .attr("width",select("#cloud_tooltip_text").node().getBBox().width)
+  //     .attr("height",select("#cloud_tooltip_text").node().getBBox().height)
+  //     .attr("x",select("#cloud_tooltip_text").node().getBBox().x)
+  //     .attr("y",select("#cloud_tooltip_text").node().getBBox().y)
+  //     .attr("fill","black")
+  //     .style("opacity",0.3);
+  // }
+  // function handleMouseOut() {
+  //   select(this)
+  //     .style("opacity",1);
+  //
+  //   //Remove tooltip
+  //   select("#cloud_tooltip_text").remove(); //get rid of tooltip text
+  //   select("#cloud_tooltip_rect").remove(); //get rid of the tooltip rect
+  // }
+  // //Create a tooltip - Greg: perhaps this can be where we create the single text box
+  // var tip = svg.append("g")
+  //   .attr("id","cloud_tooltip")
+  //   .attr("position","absolute");
 }
