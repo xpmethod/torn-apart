3857884983d074bebd92fc8f0afa81abce755eae
Roopika Risam <rrisam@salemstate.edu>
2018-08-02 19:38:12 -0400

    added switch to sidebar, still has issues

--- src/explorer/murderboard-sidebar.js
+++ src/explorer/murderboard-sidebar.js
@@ -1,6 +1,20 @@
 import { select } from "d3-selection";
 
 export default function(d){
-  select("#explorer-sidebar")
-    .html(`<h2>${d.name}`);
+  let html;
+  switch(d.category){
+  case "product":
+    html = "Product: ${d.name}";
+    break;
+  case "product category":
+    html = "Product Category: ${d.name}";
+    break;
+  case "company":
+    html = "Company: ${d.name}";
+    break;
+  case "parent company":
+    html = "Parent Company: ${d.name}";
+    break;
+  }
+  select("#explorer-sidebar").html(html);
 }
