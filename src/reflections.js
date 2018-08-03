f49baeb2a1e5e609c59be9f749f0972e026dcef9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-03 02:13:40 -0400

    wire up reflections to launch my three vizes.

--- src/reflections.js
+++ src/reflections.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
-import batwingInit from "./batwing/init";
+import nightingaleInit from "./nightingale/init";
 
 $("#nav-reflections").addClass("active");
 $(".redacted").html((i, html) => html.split(" ").map(text => `<span class="redacted-span">${text}</span>`).join(" "));
@@ -7,5 +7,5 @@ $(".footnotes").prepend("<hr><h2 class='footnotes-header'>Footnotes</h2>");
 $(".footnotes ol li p").html((i, html) => html.replace("↩", "<i class='fa fa-undo'></i>"));
 
 if($("#batwing-div").length > 0){
-  batwingInit();
+  nightingaleInit();
 }
