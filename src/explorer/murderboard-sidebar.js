f1027891f4bf75238457425141d22f019930ae1f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-04 14:51:59 -0400

    Add skeleton switch for murderboard sidebar.

--- src/explorer/murderboard-sidebar.js
+++ src/explorer/murderboard-sidebar.js
@@ -1,6 +1,25 @@
 import { select } from "d3-selection";
+// import productSidebar from "./product-sidebar";
+// import productCategorySidebar from "./product-category-sidebar";
+import companySidebar from "./company-sidebar";
+// import parentCompanySidebar from "./parent-company-sidebar";
 
 export default function(d){
-  select("#explorer-sidebar")
-    .html(`<h2>${d.name}`);
+  
+  let html;
+  switch(d.category){
+  // case "product":
+  //   html = productSidebar(d);
+  //   break;
+  // case "product category":
+  //   html = productCategorySidebar(d);
+  //   break;
+  case "company":
+    html = companySidebar(d);
+    break;
+  // case "parent company":
+  //   html = parentCompanySidebar(d);
+  //   break;
+  }
+  select("#explorer-sidebar").html(html);
 }
