842e4760a07cc2b57daa08a0dffeb4246eabc228
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-03 23:17:58 -0400

    show rose.

--- src/nightingale/init.js
+++ src/nightingale/init.js
@@ -1,9 +1,9 @@
 import batwingInit from "./batwing/init";
 import stackedBarInit from "./stacked-bar/init";
-// import roseInit from "./rose/init";
+import roseInit from "./rose/init";
 
 export default function(){
   batwingInit();
   stackedBarInit();
-  // roseInit();
+  roseInit();
 }
