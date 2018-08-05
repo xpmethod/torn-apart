eea012766ba5cb15c748d5d0e5e7dc933d7fadf5
Roopika Risam <rrisam@salemstate.edu>
2018-08-05 13:12:47 -0400

    added imports for sidebars to relay

--- src/explorer/murderboard-sidebar.js
+++ src/explorer/murderboard-sidebar.js
@@ -1,25 +1,25 @@
 import { select } from "d3-selection";
-// import productSidebar from "./product-sidebar";
-// import productCategorySidebar from "./product-category-sidebar";
+import productSidebar from "./product-sidebar";
+import productCategorySidebar from "./product-category-sidebar";
 import companySidebar from "./company-sidebar";
-// import parentCompanySidebar from "./parent-company-sidebar";
+import parentCompanySidebar from "./parent-company-sidebar";
 
 export default function(d){
-  
+
   let html;
   switch(d.category){
-  // case "product":
-  //   html = productSidebar(d);
-  //   break;
-  // case "product category":
-  //   html = productCategorySidebar(d);
-  //   break;
+  case "product":
+    html = productSidebar(d);
+    break;
+  case "product category":
+    html = productCategorySidebar(d);
+    break;
   case "company":
     html = companySidebar(d);
     break;
-  // case "parent company":
-  //   html = parentCompanySidebar(d);
-  //   break;
+  case "parent company":
+    html = parentCompanySidebar(d);
+    break;
   }
   select("#explorer-sidebar").html(html);
 }
