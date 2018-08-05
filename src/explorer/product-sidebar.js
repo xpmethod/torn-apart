eddf95dfd6575b2ef91814c6cfd320db93ec9c54
Roopika Risam <rrisam@salemstate.edu>
2018-08-05 13:08:32 -0400

    added product-sidebar

--- src/explorer/product-sidebar.js
+++ src/explorer/product-sidebar.js
@@ -2,7 +2,7 @@
 //import graph from "../../data/explorer/graph.json";
 
 export default function(d){
-  //const parentCompany = _.find(graph.links, link => link.target.name === d.name);
-  //console.log(parentCompany);
+//const parentCompany = _.find(graph.links, link => link.target.name === d.name);
+//console.log(parentCompany);
   return `<h2>you clicked on a company named ${d.name}!</h2>`;
 }
