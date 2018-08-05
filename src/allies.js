a108b805cee142c0dde732b539378e925f8c975f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-04 22:33:10 -0400

    Get allies dots and popups working.

--- src/allies.js
+++ src/allies.js
@@ -1,5 +1,11 @@
-// import $ from "jquery";
+import $ from "jquery";
 import initMap from "./map-init";
+// import alliesLegend from "./allies/legend";
+// import alliesModal from "./allies/modal";
+import alliesLayer from "./allies/layer";
+import alliesD3 from "./allies/d3";
 
-// const map = 
-initMap("allies-mapdiv");
+const map = initMap("allies-mapdiv");
+alliesLayer().addTo(map);
+alliesD3(map);
+$("#d3-allies-svg").show();
