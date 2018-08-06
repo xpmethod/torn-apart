b135f2db86c21ab56e5b623a954fb17322e954c7
Roopika Risam <rrisam@salemstate.edu>
2018-08-06 15:26:22 -0400

    tweaked product category sidebar

--- src/explorer/product-category-sidebar.js
+++ src/explorer/product-category-sidebar.js
@@ -1,9 +1,6 @@
-//import _ from "lodash";
-//import graph from "../../data/explorer/graph.json";
 
 export default function(d){
-  //const parentCompany = _.find(graph.links, link => link.target.name === d.name);
-  //console.log(parentCompany);
+
   return `<p><h4><b>Product Category:</b>
   <br><br>${d.id}`;
 }
