f24b26b098dc05e3da2b8fc4df0fe47d6aeb88ee
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-06 14:55:59 -0400

    Add tree sidebar and buttons.

---
+++ src/explorer/tree-sidebar.js
@@ -0,0 +1,15 @@
+// import { select } from "d3-selection";
+import treeSidebarPlayButtons from "./tree-sidebar-play-buttons";
+
+export default function (data){
+
+  // draw buttons
+  treeSidebarPlayButtons();
+
+  
+  // rotate timer
+
+  // treeSidebarContent
+  // select("#tree-sidebar")
+  return data;
+}
