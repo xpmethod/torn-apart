65c65781aef42af1f3d252d59f9fd15f86606bab
Roopika Risam <rrisam@salemstate.edu>
2018-08-06 16:55:32 -0400

    tweaked parent company sidebar

--- src/freezer/parent-company-sidebar.js
+++ src/freezer/parent-company-sidebar.js
@@ -3,6 +3,7 @@
 export default function(d){
   return `<p><h4><b>Parent Contractor:</b>
   <br><br>${d.name}
+  <br><br><b>ICE Awarding Office:</b> <br>${d.awards[0].awarding_office_name}
   <br><br><b>Total value of awards:</b>
   <br>$${d.total_value}</h4>`;
 }
