fc7e9618eb93d57ac3a0881ce7fd0f77dafc4130
Roopika Risam <rrisam@salemstate.edu>
2018-08-10 12:08:25 -0500

    trying to make product category sidebar work. lol forever

--- src/freezer/product-category-sidebar.js
+++ src/freezer/product-category-sidebar.js
@@ -1,11 +1,9 @@
 import _ from "lodash";
-import { bigMoneyFormat } from "../utils";
 
 export default function(d){
-  const awards = d.awards.sort((a, b) => b.current_total_value - a.current_total_value);
-  const html = ["<div class='spreadsheet'><table class='table table-hover table-sm'>"];
+  const html = ["<table class='table table-hover table-sm'>"];
   html.push("<thead><tr class='thead-dark'>");
-  _.each(["ta-v2-dba", "ta-v2-location", "ta-v2-category", "ta-v2-product-service", "ta-v2-current-value", "ta-v2-potential-value"], header => {
+  _.each(["ta-v2-product-category"], header => {
     let th = "<th>";
     if(d.color){
       th = `<th style="background-color: ${d.color};">`;
@@ -13,19 +11,11 @@ export default function(d){
     html.push(`${th}<small data-i18n="${header}"></small></th>`);
   });
   html.push("</thead><tbody>");
-  _.each(awards, award => {
+  _.each(d, d => {
     html.push("<tr>");
-    html.push(`<td><small>${award.recipient_name}</small></td>`);
-    html.push(`<td><small>${award.recipient_city}, ${award.recipient_state}</small></td>`);
-    html.push(`<td><small>${award.naics_cat}</small></td>`);
-    html.push(`<td><small>${award.naics_description}</small></td>`);
-    html.push(`<td><small>$${bigMoneyFormat(award.current_total_value)}</small></td>`);
-    html.push(`<td><small>$${bigMoneyFormat(award.potential_total_value)}</small></td>`);
+    html.push(`<td><small>${d.name}`);
     html.push("</tr><tr>");
-    html.push("<td>&nbsp;</td>");
-    html.push(`<td colspan="5" class="award-description"><small><span data-i18n="ta-description"></span>: ${award.award_description}</small></td>`);
-    html.push("</tr>");
   });
-  html.push("</tbody></table></div>");
+  html.push("</tbody></table>");
   return html.join("\n");
 }
