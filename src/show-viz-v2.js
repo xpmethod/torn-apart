42c9eb1db86f86257aaeea00b58e2903136de763
RHendery <hendery@gmail.com>
2018-08-12 00:26:53 +1000

    rolled back tooltips in lines

--- src/show-viz-v2.js
+++ src/show-viz-v2.js
@@ -14,6 +14,7 @@ export default function (viz, map){
   clearIntervals(); // kill all timed functions like banned viz.
   mapZoomDisable(map); // disable zoom by default.
   $(".viz-hide").hide(); // hide all the vizualizations.
+  $("#tooltip").css("opacity", 0); // hide the tooltip.
   switch (viz) {
   case "gain":
     gain(map);
