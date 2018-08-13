c5f81bac6341cdb5ccd2d746e3a4728d570fcf23
Roopika Risam <rrisam@salemstate.edu>
2018-08-12 23:03:13 -0400

    fixed product sidebar

--- src/freezer/product-sidebar.js
+++ src/freezer/product-sidebar.js
@@ -1,10 +1,14 @@
 //import { bigMoneyFormat } from "../utils";
 //import sideBarSpreadsheet from "./sidebar-spreadsheet";
 
+import { bigMoneyFormat } from "../utils";
+import sideBarSpreadsheet from "./sidebar-spreadsheet";
 export default function(d){
 
-  const html = [`<p><h3 style="color:#8da0cb;">Product:</h3>
-  <h4 style="color:#8da0cb;">${d.name}</h4>`];
-  //html.push(sideBarSpreadsheet(d));
+  const html = [`<h3 style="color:#8da0cb;">${d.name}</h3>`];
+  html.push(`<h4 style="color:#8da0cb;"><span data-i18n="ta-value-to-date"></span>:
+      $${bigMoneyFormat(d.total_value)}</h4>`);
+  html.push(sideBarSpreadsheet(d));
   return html.join("\n");
+
 }
