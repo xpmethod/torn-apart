00aaf1b67ea5bf4106ae056765b8fdc719d4ad7c
Roopika Risam <rrisam@salemstate.edu>
2018-08-12 23:04:30 -0400

    tidied up messy code in product category sidebar

--- src/freezer/product-category-sidebar.js
+++ src/freezer/product-category-sidebar.js
@@ -1,9 +1,3 @@
-//import _ from "lodash";
-//import sideBarSpreadsheet from "./sidebar-spreadsheet";
-
-//import { bigMoneyFormat } from "../utils";
-//import sideBarSpreadsheet from "./sidebar-spreadsheet";
-
 import { bigMoneyFormat } from "../utils";
 import sideBarSpreadsheet from "./sidebar-spreadsheet";
 export default function(d){
@@ -15,31 +9,3 @@ export default function(d){
   return html.join("\n");
 
 }
-//  const html = [`<p><h3 style="color:#fc8d62;">Product Category:</h3>
-//  <h4 style="color:#fc8d62;">${d.name}</h4>`];
-//  //html.push(sideBarSpreadsheet(d));
-//  return html.join("\n");
-//}
-
-//export default function(d){
-
-//const html = ["<table class='table table-hover table-sm'>"];
-//html.push("<thead><tr class='thead-dark'>");
-//  _.each(["ta-v2-category"], header => {
-//let th = "<th>";
-//if(d.color){
-//  th = `<th style="background-color: ${d.color};">`;
-//}
-//html.push(`${th}<small data-i18n="${d.name}"></small></th>`);
-//  });
-//html.push("</thead><tbody>");
-
-
-//  _.each(d, d => {
-//  html.push("<tr>");
-//    html.push(`<td><small>${d.name}`);
-//    html.push("</tr><tr>");
-//  });
-//html.push("</tbody></table>");
-//return html.join("\n");
-//}
