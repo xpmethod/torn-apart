24c3ff7e937326c021bd65ae8d0d3c9cad532b82
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-13 19:05:43 -0400

    Clean up districts tooltip and move tooltip styling to separate file.

--- src/utils.js
+++ src/utils.js
@@ -77,7 +77,7 @@ export function slug(string) {
 export function getOrdinalSuffix(n) {
   const suffix = ["th", "st", "nd", "rd"],
     variance = n % 100;
-  return n+(suffix[(variance - 20) % 10]||suffix[variance]||suffix[0]);
+  return suffix[(variance - 20) % 10]||suffix[variance]||suffix[0];
 }
 
 export function fillV2DivHeight(elementAbove){
