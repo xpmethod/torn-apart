85026560b99cd36d529a58b11b0fd67070d0068c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-14 19:40:20 -0400

    Rename gain display function for consistency.

--- src/gain.js
+++ src/gain.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
-import display from "./gain/display";
+import gainDisplay from "./gain/display";
 
 export default function(){
   $("#v2-div").show();
   $("#gain-viz").show();
-  display();
+  gainDisplay();
 }
