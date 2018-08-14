bdad090463f6c89982d4c05ffdc96bf16875c555
Roopika Risam <rrisam@salemstate.edu>
2018-08-14 10:56:51 -0400

    cleaned up old legend code from murderboard sidebar

--- src/freezer/murderboard-legend.js
+++ src/freezer/murderboard-legend.js
@@ -6,37 +6,31 @@ import { orange, purple, green, pink } from "../constants";
 export default function(){
   const size = 20;
   $("#legend").show().html(() => {
-    const html = ["<div class='px-3 py-2'><div><p data-i18n='ta-murderboard-legend-title'></p></div>"];// 
-   
+    const html = ["<div class='px-3 py-2'><div><p data-i18n='ta-murderboard-legend-title'></p></div>"];//
+
     html.push(`<div class="row form-group"><div class = "col-md-1">
         <svg class="legend-svg" height="${size}" width="${size}">
-          <rect height="${size}" width="${size}" class="legend-polygon" fill="${purple}" /></svg></div>`
-		  );
-		  
-	html.push("<div class ='col-md-5'><p class='axis' data-i18n='ta-parent-company'></p></div>");
-		  
+          <rect height="${size}" width="${size}" class="legend-polygon" fill="${purple}" /></svg></div>`);
+    html.push("<div class ='col-md-5'><p class='axis' data-i18n='ta-parent-company'></p></div>");
     html.push(`<div class = "col-md-1"><svg class="legend-svg" height="${size}" width="${ size}"><rect height="${size}" width="${size}" class="legend-polygon" fill="${green}"/>
           </svg></div>
         `
     );
 
-	html.push("<div class ='col-md-5'><p class='axis' data-i18n='ta-product'></p></div></div>");
-  
-    html.push(`<div class = "row form-group"><div class = "col-md-1"><svg class="legend-svg" height="${ size}" width="${size}">
-          <rect height="${size}" width="${size}" class="legend-polygon" fill="${orange}" /></svg></div>`)
-		  
-   
+    html.push("<div class ='col-md-5'><p class='axis' data-i18n='ta-product'></p></div></div>");
+    html.push(`<div class = "row form-group"><div class = "col-md-1"><svg class="legend-svg" height="${ size}" width="${size}">        <rect height="${size}" width="${size}" class="legend-polygon" fill="${orange}" /></svg></div>`);
+
     html.push("<div class='col-md-5'><p class='axis' data-i18n='ta-company'></p></div>");
-	
-	html.push(`<div class = "col-md-1"><svg class="legend-svg" height="${ size}" width="${ size}"><rect height="${size}" width="${size}" class="legend-polygon" fill="${pink}"/>
+
+    html.push(`<div class = "col-md-1"><svg class="legend-svg" height="${ size}" width="${ size}"><rect height="${size}" width="${size}" class="legend-polygon" fill="${pink}"/>
         </svg>
         </div>`);
- 
-	html.push("<div class ='col-md-5'><p class='axis' data-i18n='ta-product-category'></p></div></div>");
-	
+
+    html.push("<div class ='col-md-5'><p class='axis' data-i18n='ta-product-category'></p></div></div>");
+
     return html.join("\n");
   });
   updateTexts();
   moveLegend();
-  
+
 }
--- src/freezer/murderboard-sidebar.js
+++ src/freezer/murderboard-sidebar.js
@@ -4,10 +4,9 @@ import productCategorySidebar from "./product-category-sidebar";
 import companySidebar from "./company-sidebar";
 import parentCompanySidebar from "./parent-company-sidebar";
 import updateTexts from "../update-texts";
-//import { pink, green, purple, orange } from "../constants";
 import murderboardLegend from "./murderboard-legend";
 export default function(d){
-	
+
   murderboardLegend();
 
   let html;
@@ -25,71 +24,6 @@ export default function(d){
     html = parentCompanySidebar(d);
     break;
   }
-  
-   
-  // trying to make legend work
-
-  //  const legend = murderboardLegend(scale);
-  //  var svg = select("#murderboard-legend");
-
-  //  svg.append("g")
-  //    .classed("murderboardLegend legend", true)
-  //    .call(legend)
-  //    .attr("transform", `translate(30, ${svg.attr("height") -
-  //        $("g.murderboard-legend")[0].getBBox().height + 20})`)
-  //    .attr("fill", purple);
-
-  //  svg.append("rect")
-  //    .attr("x", 20)
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("height", 10)
-  //    .attr("width", 10)
-  //    .attr("fill", orange);
-
-  //  svg.append("text")
-  //    .text("Product Category")
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("fill", orange)
-  //    .attr("x", 40);
-
-  //  svg.append("rect")
-  //    .attr("x", 20)
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("height", 10)
-  //    .attr("width", 10)
-  //    .attr("fill", purple);
-
-  //  svg.append("text")
-  //    .text("Product")
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("fill", purple)
-  //    .attr("x", 40);
-
-  //  svg.append("rect")
-  //    .attr("x", 20)
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("height", 10)
-  //    .attr("width", 10)
-  //    .attr("fill", pink);
-
-  //  svg.append("text")
-  //    .text("Subsidiary Contractor")
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("fill", pink)
-  //    .attr("x", 40);
-
-  //  svg.append("rect")
-  //    .attr("x", 20)
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("height", 10)
-  //    .attr("width", 10)
-  //    .attr("fill", green);
-
-  //  svg.append("text")
-  //    .text("Contractor")
-  //    .attr("y", height - $("g.murderboard-legend")[0].getBBox().height - 20)
-  //    .attr("fill", green)
-  //    .attr("x", 40);
 
   select("#freezer-sidebar").html(html);
   updateTexts();
