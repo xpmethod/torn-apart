460052a87d450d08435cccee6fd6efe083c6cadd
RHendery <hendery@gmail.com>
2018-08-18 21:36:51 +1000

    de-capitates names and then fixes most of the problems that causes

--- src/clean-names.js
+++ src/clean-names.js
@@ -6,6 +6,9 @@ export default function(vendor){
   // origName: the name used in follow_the_money_data.csv
   // vendorName: the name from the internet. Can be undefined.
   // url: corporate url. Can be undefined.
+  
+  // const states = ["AK", "AZ", "AR", "CA", "CO", "CT", "FL", "GA", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "MD", "MA", "MI", "MN", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"] 
+  const stateRegex = "/(\bAK\b|\bAZ\b|\bAR\b|\bCA\b|\bCO\b|\bCT\b|\bFL\b|\bGA\b|\bID\b|\bIL\b|\bIN\b|\bIA\b|\bKS\b|\bKY\b|\bLA\b|\bMD\b|\bMA\b|\bMI\b|\bMN\b|\bMO\b|\bMT\b|\bNE\b|\bNV\b|\bNH\b|\bNJ\b|\bNM\b|\bNY\b|\bNC\b|\bND\b|\bOH\b|\bOK\b|\bOR\b|\bPA\b|\bRI\b|\bSC\b|\bSD\b|\bTN\b|\bTX\b|\bUT\b|\bVT\b|\bVA\b|\bWA\b|\bWV\b|\bWI\b|\bWY\b)/i"; //all the state abbrevs except for those that are real two-letter words that might plausibly appear in the dataset (ME, DE, MS, HI)
 
   vendor.vendorName = vendor.vendorName || vendor.origName;
 
@@ -28,14 +31,14 @@ export default function(vendor){
     // ", The". and once more to catch ", the, inc."
     .replace(/^(.*), \(?the\)?$/i, "THE $1")
     .replace(/, ?$/, "") // pesky trailing commas
-    // ", City Of" and friends.
-    .replace(/^(.*), (city|town|county|state) of$/i, "$2 OF $1")
+  //  ", City Of" and friends.
+    .replace(/^(.*), (.*) of$/i, "$2 OF $1")
   // 2. "(U.S.A.)"
     .replace(/\(u\.? ?s\.? ?a?\.? ?\)/i, "USA")
   // 3. apostrophes
     .replace(/'/g, "’");
 
-  // Non regexable:
+  // Non regexable: (some now caught with the alteration to "city of and friends" line above 
   if(vendor.cleanName === "ENVIRONMENTAL QUALITY, TEXAS COMMISSION ON"){
     vendor.cleanName = "Texas Commission on Environmental Quality";
   }
@@ -45,18 +48,18 @@ export default function(vendor){
   if(vendor.cleanName === "Mckinsey & Company, Inc. Washington D.C."){
     vendor.cleanName = "McKinsey & Company";
   }
-  if(vendor.cleanName === "MOTOR VEHICLES, NEW YORK STATE DEPARTMENT OF"){
-    vendor.cleanName = "New York State Department of Motor Vehicles";
+  /* if(vendor.cleanName === "MOTOR VEHICLES, NEW YORK STATE DEPARTMENT OF"){
+   vendor.cleanName = "New York State Department of Motor Vehicles";
   }
   if(vendor.cleanName === "Public Safety & Correctional Services, Maryland Dept Of"){
-    vendor.cleanName = "Maryland Dept. of Public Safety & Correctional Services";
+   vendor.cleanName = "Maryland Dept. of Public Safety & Correctional Services";
   }
   if(vendor.cleanName === "STATE POLICE, MICHIGAN DEPARTMENT OF"){
     vendor.cleanName = "Michigan Department of State Police";
   }
   if(vendor.cleanName === "State Police, Michigan Department Of"){
     vendor.cleanName = "Michigan Department of State Police";
-  }
+  }*/
   if(vendor.cleanName === "IMPERIAL, COUNTY"){
     vendor.cleanName = "County of Imperial";
   }
@@ -99,6 +102,7 @@ export default function(vendor){
   if(vendor.cleanName === "MacQueen, Michael C"){
     vendor.cleanName = "Michael C. MacQueen";
   }
+
   // Dunno if these are correct.
   // if(vendor.cleanName === "Senevirante, Anusha"){
   //   vendor.cleanName = "Anusha Senevirante";
@@ -107,10 +111,110 @@ export default function(vendor){
   //   vendor.cleanName = "Anthony Ibeagha";
   // }
 
-  // if(vendor.cleanName.toUpperCase() === vendor.cleanName){
-  //   // it's in ALL CAPS.
-  //   // Do stuff.
-  // }
+  if(vendor.cleanName.toUpperCase() === vendor.cleanName){
+  // it's in ALL CAPS.
+  
+    vendor.cleanName = vendor.cleanName.split(" ") 
+      .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
+      .join(" ")
+      .replace(/\bu\.? ?s\.? ?a?\.? ?\b/i, "USA") //cos now I have caused the Usa problem
+      .replace(stateRegex, function(match){return match.toUpperCase();}); //fix state abbreviations that have been lowercased
+    //this appears unnecessary now - the earlier data required it. Oh well.
+   
+   
+    //fix some of the acronyms that have been lowercased:
+   
+    //detect words that start with three consonants, as they can't be real words (except S--, Mc- and thr)
+    // Can't do anything about mid-word, since exaMPLes, truCKSTop and other real words have three or more consonants across syllable boundaries
+    var foundTripleCons = vendor.cleanName.search(/\b(?=[a-z]{3})[^aeiouy]{3}.*?\b/i);
+    if(foundTripleCons !==-1){
+      if(vendor.cleanName.substr(foundTripleCons, 2)!=="Mc" && !vendor.cleanName.substr(foundTripleCons, 2).match(/th/i)===true && !vendor.cleanName.substr(foundTripleCons, 1).match(/[sS]/)===true){ 
+        vendor.cleanName = vendor.cleanName.replace(/\b(?=[a-z]{3})[^aeiouy]{3}.*?\b/i, function(match){return match.toUpperCase();});
+     }
+     
+    }
+    /*  
+ //There's no point in this bit after all. It only catches 13 matches, and it turns out on Googling them, about half are not normally capitalised acronyms anyway (like mLINQS, or KWizCom). So might as well just brute-force them. 
+   //double consonant at start of word -- of more than 2 letters, otherwise we get US states again 
+   var foundDoubleCons = vendor.cleanName.search(/\b(?=[a-z]{2})[^aeiouy]{2}[a-z].*?\b/i);
+   if(foundDoubleCons!==-1){; 
+    if(!vendor.cleanName.substr(foundDoubleCons, 2).match(/bl|br|ch|cl|cr|dr|fl|fr|gl|gr|kn|pl|ph|pr|mc|sc|sh|sk|sl|sm|sn|sp|st|sq|sw|th|tr|tw|wh|wr|mr|ms/)===false){ //these are allowed initial consonant clusters in English words. If it's not one of these, it's probably an acronym
+      vendor.cleanName = vendor.cleanName.replace(/\b(?=[a-z]{2})[^aeiouy]{2}[a-z].*?\b/i,function(match){return match.toUpperCase();}); 
+    }
+   }
+*/   
+   
+
+  }
+  vendor.cleanName = vendor.cleanName.replace(/(-[a-z])/, function(match){return match.toUpperCase();}); //deals with things like Outlook-nebraska. We could handle them by expanding the split ' ' to include - but there are also examples like Washington-brede that were already problematic and not all caps initially.
+  
+  
+  if(vendor.cleanName === "Mlinqs"){
+    vendor.cleanName = "mLINQS";
+  }
+  if(vendor.cleanName === "Pma-13"){
+    vendor.cleanName = "PMA-13";
+  }
+  if(vendor.cleanName === "Csi Aviation"){
+    vendor.cleanName = "CSI Aviation";
+  }
+  if(vendor.cleanName === "Cbt Nuggets"){
+    vendor.cleanName = "CBT Nuggets";
+  }
+  
+  if(vendor.cleanName === "Mmi Outdoor"){
+    vendor.cleanName = "MMI Outdoor";
+  }
+
+  if(vendor.cleanName === "Npee"){
+    vendor.cleanName = "NPEE";
+  }
+  if(vendor.cleanName === "Kwizcom Corporation"){
+    vendor.cleanName = "KWizCom Corporation";
+  }
+  
+  if(vendor.cleanName === "Cookie's Dme"){
+    vendor.cleanName = "Cookie's DME";
+  }
+
+  if(vendor.cleanName === "Jsi Telecom"){
+    vendor.cleanName = "JSI Telecom";
+  }
+  if(vendor.cleanName === "Cjen"){
+    vendor.cleanName = "CJEN";
+  }
+
+  if(vendor.cleanName === "Lc Industries"){
+    vendor.cleanName = "LC Industries";
+  }
+
+  if(vendor.cleanName === "Palantir Usg"){
+    vendor.cleanName = "Palantir USG";
+  }  
+  
+  if(vendor.cleanName === "4imprint"){
+    vendor.cleanName = "4Imprint";
+  }  
+ 
+  if(vendor.cleanName === "Faac"){
+    vendor.cleanName = "FAAC";
+  } 
+
+  if(vendor.cleanName === "Msab"){
+    vendor.cleanName = "MSAB";
+  }
+  
+  if(vendor.cleanName === "Dialtoneservices"){
+    vendor.cleanName = "DialToneServices";
+  }
+  if(vendor.cleanName === "Green It Systems Group"){
+    vendor.cleanName = "Green IT Systems Group";
+  }
+
+  
+  vendor.cleanName.replace("Kcorp", "KCorp"); //this one turns up inside a couple of different names
+
+  vendor.cleanName.replace("At&t", "AT&T");
 
 
   // Global capitalization, since even mixed names often don't
@@ -126,6 +230,7 @@ export default function(vendor){
     .replace(/^the /i, "The ");
 
   stdout.write(`${vendor.cleanName}          ---- (${vendor.vendorName})\n`);
+
 
   return vendor;
 }
