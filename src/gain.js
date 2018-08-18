94640b308573f8cd12024a150639f9e3ccada081
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-17 22:51:01 -0400

    Add carousel to gain.

--- src/gain.js
+++ src/gain.js
@@ -1,8 +1,12 @@
 import $ from "jquery";
 import gainDisplay from "./gain/display";
+import gainCarousel from "./gain/carousel";
 
 export default function(){
   $("#v2-div").show();
   $("#gain-viz").show();
   gainDisplay();
+  gainCarousel();
 }
+
+
