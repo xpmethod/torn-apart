cba7d562c41240c05ea33a4ad195a91b4c64a6c5
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-19 22:09:06 -0400

    Start thining out districts.

--- src/districts/write-geojson.js
+++ src/districts/write-geojson.js
@@ -4,32 +4,39 @@ import { stdout } from "process";
 import path from "path";
 import _ from "lodash";
 
-export default function(districts){
+export default function(rawDistricts){
   const geoJSON = JSON.parse(
     readFileSync(path.join("src", "districts", "congressional-districts.geo.json"))
   );
-  _.each(geoJSON.features, shape => {
-    const district = _.find(districts, { id2: shape.properties.GEOID.replace(/^0/, "") });
-    if(district){
-      shape.properties.display_label = district.display_label;
-      shape.properties.party = district.party;
-      shape.properties.representative = district.representative;
-      shape.properties.representative_photo_url = district.representative_photo_url;
-      shape.properties.fy13 = district.fy13;
-      shape.properties.fy14 = district.fy14;
-      shape.properties.fy15 = district.fy15;
-      shape.properties.fy16 = district.fy16;
-      shape.properties.fy17 = district.fy17;
-      shape.properties.fy18 = district.fy18;
-      shape.properties.awards = district.awards;
-      shape.properties.total_value = district.total_awards;
-      shape.properties.district_url = district.district_url;
-    } else {
-      stdout.write(`Could not find ${shape.properties.GEOID}\n`);
-    }
+  const districts = rawDistricts.filter(d => d.total_awards > 0);
+  stdout.write(`there are ${districts.length} districts.\n`);
+
+  const features = _.map(districts, district => {
+    const feature = {
+      type: "Feature"
+    };
+    feature.geometry = _.filter(geoJSON.features, shape => {
+      return shape.properties.GEOID.replace(/^0/, "") === district.id2;
+    })[0].geometry;
+    feature.properties = {
+      display_label: district.display_label,
+      party: district.party,
+      representative: district.representative,
+      representative_photo_url: district.representative_photo_url,
+      fy13: district.fy13,
+      fy14: district.fy14,
+      fy15: district.fy15,
+      fy16: district.fy16,
+      fy17: district.fy17,
+      fy18: district.fy18,
+      awards: district.awards,
+      total_value: district.total_awards,
+      district_url: district.district_url,
+    };
+    return feature;
   });
   writeFile(path.join("data", "districts", "fat_districts.geo.json"), 
-    JSON.stringify(geoJSON, null, 2), (err) => {
+    JSON.stringify({ type: "FeatureCollection", features }, null, 2), (err) => {
       if(err) throw err;
       stdout.write("WE DID THE GEOJSON 🌎\n");
     });
