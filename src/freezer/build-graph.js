c4b104405378f8b8ccc0e0088e8bf3ff88df47e8
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-19 23:38:48 -0400

    move freezer downfield

--- src/freezer/build-graph.js
+++ src/freezer/build-graph.js
@@ -1,43 +1,35 @@
-import { writeFile, readFile } from "fs";
+import { writeFile, readFileSync, readFile } from "fs";
 import { stdout } from "process";
 import path from "path";
 import parse from "csv-parse";
+import parseSync from "csv-parse/lib/sync";
 import _ from "lodash";
 
 export default function(){
   readFile(path.join("data", "follow_the_money_data.csv"), (err, data) => {
-  // readFile(path.join("data", "freezer", "freezer.csv"), (err, data) => {
     if(err) throw err;
     parse(data, {columns: true}, (err, rawAwards) => {
       if(err) throw err;
+      const decorations = parseSync(
+        readFileSync(path.join("data", "vendor_decorations.csv")),
+        { columns: true });
+      const product_taxonomy = parseSync(
+        readFileSync(path.join("data", "products_taxonomy.csv")),
+        { columns: true });
+      _.each(product_taxonomy, row => { row.tas_taxonomy = row.tas_taxonomy.trim(); });
+      // const product_categories = _.cloneDeep(product_taxonomy);
       const awards = rawAwards.filter(award => award.fiscal_year === "2018");
       const companies = [];
       const products = [];
-      const product_categories = [
-        "Those that belong to the emperor",
-        "Embalmed ones",
-        "Those that are trained",
-        "Suckling pigs",
-        "Mermaids (or Sirens)",
-        "Fabulous ones",
-        "Stray dogs",
-        "Those that are included in this classification",
-        "Those that tremble as if they were mad",
-        "Innumerable ones",
-        "Those drawn with a very fine camel hair brush",
-        "Et cetera",
-        "Those that have just broken the flower vase",
-        "Those that, at a distance, resemble flies"
-      ];
-      // const product_categories = [];
       const parent_companies = [];
+      const product_categories = [];
       _.each(awards, (award) => {
-        award.naics_cat = _.shuffle(product_categories)[0];
+        award.naics_cat = _.find(product_taxonomy, { "naics_description": award.naics_description }).tas_taxonomy;
         award.product_combo = award.naics_description + "||" + award.naics_cat;
-        companies.push(award.recipient_duns);
+        award.company_combo = award.recipient_duns + "||" + award.recipient_parent_duns;
+        companies.push(award.company_combo);
         products.push(award.product_combo);
-        // products.push(award.naics_description);
-        // product_categories.push(award.product_category);
+        product_categories.push(award.naics_cat);
         parent_companies.push(award.recipient_parent_duns);
       }); // close each
       const companies_uniq = _.uniq(companies);
@@ -50,29 +42,27 @@ export default function(){
       _.each([
         { source_array: parent_companies_uniq,
           category: "parent company",
-          // combo_col: "company_combo",
           source_column: "recipient_parent_duns",
-          target_column: "recipient_duns"
+          target_column: "company_combo"
+          // target_column: "recipient_duns"
         },
         { source_array: product_categories_uniq,
           category: "product category",
-          // combo_col: "product_combo",
           source_column: "naics_cat",
           target_column: "product_combo"
         },
         { source_array: companies_uniq,
-          // combo_col: "company_combo",
           category: "company",
-          source_column: "recipient_duns",
+          source_column: "company_combo",
           target_column: "product_combo",
-          // target_column: "naics_description",
-          // combo: true
+          combo: true
         }
       ], (sources) => {
         _.each(sources.source_array, (source) => {
           const name = sources.combo ? source.split("||")[0] : source;
           const childOf = sources.combo ? source.split("||")[1] : null;
-          graph.nodes.push({ id: source, name, childOf, category: sources.category });
+          const displayName = name.match(/^\d*$/) ? _.find(decorations, { duns: name }).cleanName : name;
+          graph.nodes.push({ id: source, name, childOf, category: sources.category, displayName });
           const targets_all = awards.map((award) => {
             if(award[sources.source_column] === source){
               return award[sources.target_column];
@@ -93,9 +83,10 @@ export default function(){
         const name = product.split("||")[0];
         const childOf = product.split("||")[1];
         const productAwards = _.filter(awards,
-          { naics_description: name, naics_cat: childOf });
+          { naics_description: name, naics_cat: childOf })
+          .map(thinAward);
         const total_value = productAwards.reduce( (sum, award) => {
-          return sum + _.toInteger(award.current_total_value);
+          return sum + _.toInteger(award.current_total_value_of_award);
         }, 0);
         graph.nodes.push({ name,
           childOf,
@@ -112,17 +103,19 @@ export default function(){
 
       _(graph.nodes.filter(node => node.category === "parent company"))
         .each(parentCompany => {
-          parentCompany.awards = awards.filter(award => award.parent_name === parentCompany.name);
+          parentCompany.awards = awards.filter(award => award.recipient_parent_duns === parentCompany.name)
+            .map(thinAward);
           parentCompany.total_value = parentCompany.awards.reduce( (sum, award) => {
-            return sum + _.toInteger(award.current_total_value);
+            return sum + _.toInteger(award.current_total_value_of_award);
           }, 0);
         });
 
       _(graph.nodes.filter(node => node.category === "company"))
         .each(company => {
-          company.awards = awards.filter(award => award.recipient_name === company.name);
+          company.awards = awards.filter(award => award.recipient_duns === company.name)
+            .map(thinAward);
           company.total_value = company.awards.reduce( (sum, award) => {
-            return sum + _.toInteger(award.current_total_value);
+            return sum + _.toInteger(award.current_total_value_of_award);
           }, 0);
         });
 
@@ -132,17 +125,18 @@ export default function(){
           _(graph.links)
             .filter(link => link.source === companyNode.id)
             .each(companyLink => {
-              companyLink.contract_value = _.reduce(awards.filter(award => award.recipient_name === companyNode.name && award.product_combo === companyLink.target),
+              companyLink.contract_value = _.reduce(awards.filter(award => award.recipient_duns === companyNode.name && award.product_combo === companyLink.target),
                 (sum, award) => {
-                  return sum + _.toInteger(award.current_total_value);
+                  return sum + _.toInteger(award.current_total_value_of_award);
                 }, 0);
             });
 
           _(graph.nodes.filter(node => node.category === "product category"))
             .each(productCategory => {
-              productCategory.awards = awards.filter(award => award.naics_cat === productCategory.name);
+              productCategory.awards = awards.filter(award => award.naics_cat === productCategory.name)
+                .map(thinAward);
               productCategory.total_value = productCategory.awards.reduce( (sum, award) => {
-                return sum + _.toInteger(award.current_total_value);
+                return sum + _.toInteger(award.current_total_value_of_award);
               }, 0);
             });
 
@@ -154,7 +148,7 @@ export default function(){
                 .each(productCategoryLink => {
                   productCategoryLink.contract_Value = _.reduce(awards.filter(award => award.naics_cat === productCategoryNode.name && award.product_combo === productCategoryLink.target),
                     (sum, award) => {
-                      return sum + _.toInteger(award.current_total_value);
+                      return sum + _.toInteger(award.current_total_value_of_award);
                     }, 0);
                 });
 
@@ -169,6 +163,19 @@ export default function(){
           stdout.write("WE DID THE FREEZER THING 🚀\n");
         }); // close writeFile callback.
 
+      function thinAward(award){
+        return {
+          current_total_value_of_award: award.current_total_value_of_award,
+          award_description: award.award_description,
+          recipient_city: _.capitalize(award.recipient_city_name),
+          recipient_state: award.recipient_state_code,
+          recipient_duns: award.recipient_duns,
+          recipient_name: _.find(decorations, { duns: award.recipient_duns }).cleanName
+        };
+      }
+
     }); // close parse;
   }); // close readFile
+
 }
+
