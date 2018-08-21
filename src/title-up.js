07c7e487f3001789726e542994462989e77af726
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-21 09:12:46 -0400

    export title-up

--- src/title-up.js
+++ src/title-up.js
@@ -1,7 +1,7 @@
 import _ from "lodash";
 // var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
 
-export function titleUp(string) {
+export default function (string) {
   return _.words(string).map( word => _.capitalize(word)).join(" ");
 }
 
