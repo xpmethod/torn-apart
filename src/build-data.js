0dd83558bb77c6b2f66920e33648ad2a65e16dd7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-22 15:42:55 -0400

    Update internationalization.

--- src/build-data.js
+++ src/build-data.js
@@ -1,17 +1,17 @@
-// import generateLanguageFiles from "./i18n/generate-language-files";
+import generateLanguageFiles from "./i18n/generate-language-files";
 import rainBuildData from "./rain/build-rain-data";
-import buildDecoratorTable from "./build-decorator-table";
 import districtsBuildAllData from "./districts/build-all-contracts";
 import districtsBindData from "./districts/bind-contracts-to-districts";
 import freezerBuildGraph from "./freezer/build-graph";
-// import gainBuildData from "./gain/build-data";
+import buildDecoratorTable from "./build-decorator-table";
+import gainBuildData from "./gain/build-data";
 
 buildDecoratorTable(decorations => {
   districtsBuildAllData(districtsBindData(decorations));
   freezerBuildGraph(decorations);
   rainBuildData(decorations);
+  gainBuildData(decorations);
 });
-// generateLanguageFiles();
-// gainBuildData();
+generateLanguageFiles();
 
 
