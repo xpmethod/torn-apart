3e6912aaacc739257a7416495e5e80dd3d6e2b88
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-25 14:53:49 -0400

    Make lines scale mobile aware.

--- src/lines/draw-scale.js
+++ src/lines/draw-scale.js
@@ -1,3 +1,4 @@
+import L from "leaflet";
 import { select } from "d3-selection";
 import { axisBottom } from "d3-axis";
 import linesConstants from "./constants";
@@ -7,7 +8,7 @@ export default function() {
   // nb, y becomes x.
   const x = linesLogScale();
   const svg = select("#lines-scale-svg");
-  svg
+  const path = svg
     .append("path")
     .attr("id", "lines-scale-wedge")
     .attr("fill", "black")
@@ -16,17 +17,39 @@ export default function() {
       "d",
       `M0 0 H ${linesConstants.rangeMax} V -${linesConstants.barWidth} Z`
     );
-  svg
+  const axis = svg
     .append("g")
     .attr("id", "lines-scale-axis")
     .attr("transform", `translate(5, ${linesConstants.barWidth + 5})`)
-    .call(axisBottom(x).tickArguments([5, ".0s"]))
+    .call(axisBottom(x).tickArguments([5, ".0s"]));
+  axis
     .select(".tick")
     .select("text")
     .text("0");
-  svg
+  const removals = svg
     .append("text")
     .attr("transform", "translate(5, 10)")
     .attr("data-i18n", "ta-removals")
     .text("Removals");
+  if (L.Browser.mobile) {
+    axis.attr(
+      "transform",
+      `translate(${(svg.attr("width") - linesConstants.rangeMax) / 2}, ${
+        linesConstants.barWidth
+      })`
+    );
+    path.attr(
+      "transform",
+      `translate(${(svg.attr("width") - linesConstants.rangeMax) / 2}, ${
+        linesConstants.barWidth
+      })`
+    );
+    removals
+      .style("text-anchor", "end")
+      .attr(
+        "transform",
+        `translate(${(svg.attr("width") - linesConstants.rangeMax) / 2 -
+          10}, 10)`
+      );
+  }
 }
