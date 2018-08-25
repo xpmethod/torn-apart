e8b7e8b126b6a9d8cfe2163251797447a6af312e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-25 14:19:05 -0400

    Fix up tooltip positioning.

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,9 +1,18 @@
 import $ from "jquery";
+import L from "leaflet";
 import { event, selectAll, select } from "d3-selection";
+import { rem } from "./constants";
 
 export function handleMouseOver(d) {
   let pageX, pageY;
-  if (d.coords) {
+  let minWidth = 10 * rem;
+  let maxWidth = 28 * rem;
+  if (L.Browser.mobile) {
+    pageX = 1 * rem;
+    pageY = 4.5 * rem;
+    minWidth = $(window).width() - 2 * rem;
+    maxWidth = $(window).width() - 2 * rem;
+  } else if (d.coords) {
     pageX = d.coords[0];
     pageY = d.coords[1];
   } else {
@@ -12,7 +21,7 @@ export function handleMouseOver(d) {
   }
   if (pageY < 0) {
     pageX = 30;
-    pageY = 30;
+    pageY = 100;
   }
   d.mouseOver();
   selectAll(".tooltip").remove();
@@ -21,7 +30,8 @@ export function handleMouseOver(d) {
     .classed("tooltip", true)
     .attr("id", "tooltip")
     .style("opacity", 0.9)
-    .html(d.tooltip)
+    .style("max-width", maxWidth + "px")
+    .style("min-width", minWidth + "px")
     .style("left", function() {
       const toolTipWidth = $("#tooltip").width();
       if (toolTipWidth > pageX) {
@@ -31,13 +41,19 @@ export function handleMouseOver(d) {
       }
     })
     .style("top", function() {
-      const toolTipHeight = $("#tooltip").height();
-      if ($(window).height() - pageY < toolTipHeight) {
-        return pageY - toolTipHeight + "px";
-      } else {
+      if (L.Browser.mobile) {
         return pageY + "px";
+      } else {
+        const toolTipHeight = $("#tooltip").height();
+        // this is just going to be 3rem or 32px…
+        if ($(window).height() - pageY < 12 * toolTipHeight) {
+          return pageY - 6 * toolTipHeight + "px";
+        } else {
+          return pageY - toolTipHeight + "px";
+        }
       }
-    });
+    })
+    .html(d.tooltip);
   // .transition().delay(0).duration(0)
 }
 
