7f4acb316fc10f540c5fffc0579ee0dc20595f2f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-08-26 21:28:21 -0400

    Add districts viz to my reflection.

--- src/nightingale/init.js
+++ src/nightingale/init.js
@@ -2,10 +2,12 @@ import batwingInit from "./batwing/init";
 import stackedBarInit from "./stacked-bar/init";
 import roseInit from "./rose/init";
 import circlesInit from "./circles/init";
+import districtsInit from "./districts/init";
 
 export default function() {
   batwingInit();
   stackedBarInit();
   roseInit();
   circlesInit();
+  districtsInit();
 }
