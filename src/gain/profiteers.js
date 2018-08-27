47be3a03a786273449a65667411d1bedf4526e35
Roopika Risam <rrisam@salemstate.edu>
2018-08-27 15:25:49 +0100

    removed profiteers from gain legend

--- src/gain/profiteers.js
+++ src/gain/profiteers.js
@@ -2,7 +2,7 @@ import { select } from "d3-selection";
 import { rem, green, orange, pink, lime, beige, tan } from "../constants";
 // import gainBarsLegend from "./bars-legend";
 // import getDecoration from "../get-decoration";
-import { bigMoneyFormat } from "../utils";
+//import { bigMoneyFormat } from "../utils";
 import Data from "../../data/gain/minority-data.json";
 
 export default function() {
@@ -18,18 +18,15 @@ export default function() {
     .append("div")
     .attr("class", "d-flex justify-content-start")
     .html((d, i) => {
-      const profiteer = Data.biggestProfiteers[d];
+      //const profiteer = Data.biggestProfiteers[d];
       // const company = getDecoration(profiteer.duns);
       return `<div class="mr-2" style="max-width: 1.25rem;"><svg width="${rem}" height="${rem}">
-          <rect width="${rem}" height="${rem}" 
+          <rect width="${rem}" height="${rem}"
           transform="translate(0,${0})" fill="${colors[i]}">
         </svg></div>
         <div>
           <h4>
-            <small><span data-i18n="ta-${d}"></span>:</small>
-            ${profiteer.name}, <strong>$${bigMoneyFormat(
-        profiteer.value
-      )}</strong>
+            <span data-i18n="ta-${d}"></span>
           </h4>
         </div>`;
     });
