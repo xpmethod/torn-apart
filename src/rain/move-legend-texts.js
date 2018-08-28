d527463b2d722251d37baf8a0c069ece0fa958eb
RHendery <hendery@gmail.com>
2018-08-28 19:46:12 +1000

    labels on rain legend better aligned

--- src/rain/move-legend-texts.js
+++ src/rain/move-legend-texts.js
@@ -2,16 +2,17 @@ import $ from "jquery";
 
 export default function() {
   const x = $("#rain-size-legend")[0].getBBox().width - 5;
+  const y = $("#rain-size-legend")[0].getBBox().height/4;
   const lineHeight = 1.1;
   $("#rain-legend-unique").attr(
     "transform",
-    `translate(${x},-${($("#rain-legend-unique").children().length + 1) *
+    `translate(${x},${($("#rain-legend-unique").children().length + 1) *
       lineHeight *
-      10})`
+      10-y})`
   );
   $("#rain-legend-renewed").attr(
     "transform",
-    `translate(${x},0)`
+    `translate(${x},${y})`
     // `translate(${x},${$("#rain-legend-renewed").children().length *
     //   lineHeight *
     //   10})`
