a26698c9b3f5c45e88453ba4c93a1266b3bc81e3
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-09-03 14:48:42 -0400

    Adjust leaflet maps.

--- docs/app/components/leaflet-background.js
+++ docs/app/components/leaflet-background.js
@@ -4,6 +4,9 @@ export default Component.extend({
   lat: 0,
   lng: 0,
   zoom: 5,
+  zoomControl: false,
   tileLayerUrl:
-    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  attribution:
+    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
 });
